68a943c5cff715ada79b141a3f7dc97a018214c9
Manuel Manzanera Aroca <manuelmanzanera@ohmyapps.es>
2013-11-05 21:13:25 +0100

    first commit

--- URLSchemes/ViewController.m
+++ URLSchemes/ViewController.m
@@ -20,6 +20,47 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+    - (IBAction)webAction:(id)sender
+    {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.ohmyapps.es.es"]];
+    }
+    
+    - (IBAction)mailAction:(id)sender
+    {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"mailto:info@ohmyapps.es"]];
+    }
+    
+    - (IBAction)phoneAction:(id)sender
+    {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"tel:968813626"]];
+    }
+    
+    - (IBAction)mapAction:(id)sender
+    {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://maps.apple.com/?q=murcia"]];
+    }
+    
+    - (IBAction)iTunesAction:(id)sender
+    {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://itunes.apple.com/es/album/dont-stop-believin/id169003304?i=169003415"]];
+    }
+    
+    - (IBAction)smsAction:(id)sender
+    {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"sms:666813626"]];
+    }
+    
+    - (IBAction)faceTime:(id)sender
+    {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"facetime://info@ohmyapps.es"]];
+    }
+    
+    - (IBAction)openApp:(id)sender
+    {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"padelphone://com.ohmyapps.es"]];
+    }
+
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
